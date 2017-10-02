acc338747b17bb67552359554758059c8eb2d692
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2017-10-02 20:15:35 +0700

    Upgrade to a fuller prototype

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
-(function(){
+(function(){window.onload = function() {
 
     var BUFFER_SIZE = 256,
-        THRESHOLD   = 25,
+        THRESHOLD   = null,
+        RELATIVE_THRESHOLD = 12.0,
         MAX_DIT_MILLIS = 100,
         MAX_DAH_MILLIS = 300,
-        MAX_BLANK_MILLIS = 1000;
+        MAX_BLANK_MILLIS = 1000,
+        MAX_WORD_BLANK_MILLIS = 3000;
 
     var context = new AudioContext();
 
@@ -16,8 +18,64 @@
 
     var currentLetter = [],
         lastPulse     = 0,
-        isPeak        = false;
-    
+        isPeak        = false,
+        isLetter      = false,
+        isWord        = false;
+
+    var morseTable = {
+        '.-':   'A',
+        '-...': 'B',
+        '-.-.': 'C',
+        '-..':  'D',
+        '.':    'E',
+        '..-.': 'F',
+        '--.':  'G',
+        '....': 'H',
+        '..':   'I',
+        '.---': 'J',
+        '-.-':  'K',
+        '.-..': 'L',
+        '--':   'M',
+        '-.':   'N',
+        '---':  'O',
+        '.--.': 'P',
+        '--.-': 'Q',
+        '.-.':  'R',
+        '...':  'S',
+        '-':    'T',
+        '..-':  'U',
+        '...-': 'V',
+        '.--':  'W',
+        '-..-': 'X',
+        '-.--': 'Y',
+        '--..': 'Z',
+
+        '.----': '1',
+        '..---': '2',
+        '...--': '3',
+        '....-': '4',
+        '.....': '5',
+        '-....': '6',
+        '--...': '7',
+        '---..': '8',
+        '----.': '9',
+        '-----': '0',
+
+        '': ' '
+    };
+
+    var currentLetterDiv = document.getElementById('current-letter');
+    var wholeTextDiv = document.getElementById('whole-text');
+
+    var displayCurrentLetter = function (currentLetter) {
+        currentLetterDiv.innerText = currentLetter.join('') || ' ';
+    };
+
+    var showLetter = function (currentLetter) {
+        var letter = morseTable[currentLetter.join('')] || '?';
+        wholeTextDiv.innerText += letter;
+    };
+
     var processAudio = function (event) {
         requestAnimationFrame(processAudio);
 
@@ -28,9 +86,13 @@
             sumAbs += Math.abs(buffer[i]);
         }
 
+        if (! THRESHOLD) THRESHOLD = sumAbs * RELATIVE_THRESHOLD;
+
         if (sumAbs >= THRESHOLD && !isPeak) {
             /* rise */
             isPeak = true;
+            isLetter = true;
+            isWord = true;
         } else if (sumAbs < THRESHOLD && isPeak) {
             /* fall */
             isPeak = false;
@@ -41,16 +103,42 @@
             if (delta <= MAX_DIT_MILLIS) {
                 /* no-op */
             } else if (delta <= MAX_DAH_MILLIS) {
-                currentLetter[currentLetter.length - 1] = 'dah';
+                currentLetter[currentLetter.length - 1] = '-';
+                displayCurrentLetter(currentLetter);
             } else if (delta <= MAX_BLANK_MILLIS) {
-                currentLetter.push('dit')
+                currentLetter.push('.');
+                displayCurrentLetter(currentLetter);
             } else {
-                currentLetter = ['dit'];
+                currentLetter = ['.'];
+                displayCurrentLetter(currentLetter);
             }
 
             lastPulse = time;
 
             console.log(currentLetter);
+
+        } else if (!isPeak && isLetter) {
+            /* letter break */
+            var time = Date.now();
+            var delta = time - lastPulse;
+
+            if (delta > MAX_BLANK_MILLIS) {
+                showLetter(currentLetter);
+                isLetter = false;
+                currentLetter = [];
+                console.log(currentLetter);
+                displayCurrentLetter(currentLetter);
+            }
+            
+        } else if (!isPeak && isWord) {
+            /* word break */
+            var time = Date.now();
+            var delta = time - lastPulse;
+
+            if (delta > MAX_WORD_BLANK_MILLIS) {
+                isWord = false;
+                showLetter([]);
+            }
         }
     };
 
@@ -72,4 +160,4 @@
         console.log
     );
 
-})()
+}})()
